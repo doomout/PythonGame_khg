946145352f09999cf6bf18bf0988bdd67e645e30
khg <winquake3@hanmail.net>
2023-01-27 23:13:15 +0900

    canvas에 배경 화면 그리기

---
+++ Game_01/list0103_1.py
@@ -0,0 +1,9 @@
+import tkinter
+
+root = tkinter.Tk()
+root.title("Canvas에 화면 그리기")
+canvas = tkinter.Canvas(width=480, height=300)
+canvas.pack()
+img_bg = tkinter.PhotoImage(file="Game_01\park.png") #vs code 에선 폴더 경로도 써줘야 한다.
+canvas.create_image(240, 150, image=img_bg)
+root.mainloop()
---
+++ Game_01/list0103_2.py
@@ -0,0 +1,20 @@
+import tkinter
+
+x = 0
+def scroll_bg():
+    global x
+    x = x + 1 
+    if x == 480: # x가 480이 되면
+        x = 0 # x에 0 대입
+    canvas.delete("BG") #먼저 배경 이미지 삭제
+    canvas.create_image(x - 240, 150, image=img_bg, tag="BG") #배경 이미지 그리기(왼쪽)
+    canvas.create_image(x + 240, 150, image=img_bg, tag="BG") #배경 이미지 그리기(오른쪽)
+    root.after(50, scroll_bg) #50밀리 호 후 함수 재실행
+
+root = tkinter.Tk()
+root.title("화면 스크롤")
+canvas = tkinter.Canvas(width=480, height=300)
+canvas.pack()
+img_bg = tkinter.PhotoImage(file="Game_01\park.png")
+scroll_bg()
+root.mainloop()
