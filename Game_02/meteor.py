5cd95e48876f4f74aa4e691e137e08e088a33dca
khg <winquake3@hanmail.net>
2023-02-02 00:17:49 +0900

    주석 추가

--- Game_02/meteor.py
+++ Game_02/meteor.py
@@ -3,34 +3,35 @@ import random
 
 fnt1 = ("Times New Roman", 24)
 fnt2 = ("Times New Roman", 50)
-index = 0
-timer = 0
-score = 0
-bg_pos=0
-px = 240
-py = 540
-METEO_MAX = 30
-mx = [0] * METEO_MAX
-my = [0] * METEO_MAX
+index = 0 #게임 진행 관리
+timer = 0 #게임 진행 관리
+score = 0 #점수
+bg_pos=0 #배경 스크롤을 위한 좌표
+px = 240 #우주선 좌표
+py = 540 #우주선 좌표
+METEO_MAX = 30 #화면에 흐르는 유성 수
+mx = [0] * METEO_MAX #유성 좌표
+my = [0] * METEO_MAX #유성 좌표
 
 key = ""
 koff = False
-def key_down(e):
+def key_down(e): #키 눌렀을 때 실행
     global key, koff
     key = e.keysym
     koff = False
     
-def key_up(e):
+def key_up(e): # 키를 눌렀다 땠을 때 실행
     global koff
     koff = True
-    
-def main():
+   
+def main(): #메인 처리 실행
     global key, koff, index, timer, score, bg_pos, px
     timer = timer + 1
     bg_pos = (bg_pos + 1) % 640
     canvas.delete("SCREEN")
     canvas.create_image(240, bg_pos - 320, image=img_bg, tag="SCREEN")
     canvas.create_image(240, bg_pos + 320, image=img_bg, tag="SCREEN")
+     #0: 타이틀 화면 - 스페이스 키를 누르면 각 변수를 초기화 하고 1로 이동
     if index == 0:
         canvas.create_text(240, 240, text="METEOR", fill="gold",font=fnt2, tag="SCREEN")
         canvas.create_text(240, 480, text="Press [SPACE] Key", fill="lime",font=fnt1, tag="SCREEN")
@@ -39,10 +40,12 @@ def main():
             px = 240
             init_enemy()
             index = 1
+    #1: 게임 플레이 화면
     if index == 1:
         score = score + 1
-        move_player()
-        move_enemy()
+        move_player() #우주선 이동
+        move_enemy() #유성 이동
+    #게임 오버 화면
     if index == 2:
         move_enemy()
         canvas.create_text(240,timer * 4, text="GAME OVER", fill="red", font=fnt2, tag="SCREEN")
@@ -55,17 +58,17 @@ def main():
         koff = False
     root.after(50, main)
         
-def hit_check(x1, y1, x2, y2):
+def hit_check(x1, y1, x2, y2): #두 점 사이 거리로 히트 체크 수행
     if((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2) < 36 * 36):
         return True
     return False
 
-def init_enemy():
+def init_enemy(): #유성 좌표 초기화
     for i in range(METEO_MAX):
         mx[i] = random.randint(0, 480)
         my[i] = random.randint(-640, 0)
     
-def move_enemy():
+def move_enemy(): #유성 이동
     global index, timer
     for i in range(METEO_MAX):
         my[i] = my[i] + 6 + i /5
@@ -77,7 +80,7 @@ def move_enemy():
             timer = 0
         canvas.create_image(mx[i],my[i], image=img_enemy, tag="SCREEN")
     
-def move_player():
+def move_player(): #우주선 이동
     global px
     if key == "Left" and px>30:
         px = px - 10
